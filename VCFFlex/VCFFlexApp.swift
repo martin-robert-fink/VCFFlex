caff244bb06e4b6bb20169665f39dd8b8f12740a
Martin Fink <martin.robert.fink@gmail.com>
2026-03-16 13:17:10 -0600

    Initial Commit

---
+++ VCFFlex/VCFFlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  VCFFlexApp.swift
+//  VCFFlex
+//
+//  Created by Martin Fink on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct VCFFlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
